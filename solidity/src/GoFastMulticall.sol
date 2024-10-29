0b90eba94ad0237af9507fdff0c1744a70aa707d
thal0x <thal0x.eth@gmail.com>
2024-10-29 06:12:43 -0500

    use forceApprove for erc20 token allowances

--- solidity/src/GoFastMulticall.sol
+++ solidity/src/GoFastMulticall.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract GoFastCaller {
+    using SafeERC20 for IERC20;
+
     function execute(address _target, address _token, uint256 _amount, bytes memory _data)
         external
         returns (bool, bytes memory)
     {
-        IERC20(_token).approve(_target, _amount);
+        IERC20(_token).forceApprove(_target, _amount);
 
         (bool success, bytes memory returnData) = _target.call(_data);
         if (!success) {
